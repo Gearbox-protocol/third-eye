8089f23f09d13690f32f8190ec2978aa80e47eb2
mikael <mikael.lazarev@gmail.com>
2021-12-09 21:18:58 +0100

    config update

--- config/config.go
+++ config/config.go
@@ -12,23 +12,18 @@ type Config struct {
 
 	// Database
 	DatabaseUrl string `env:"DATABASE_URL" validate:"required"`
-	Domain      string `env:"DOMAIN"`
 
 	// Environment
 	Env string `env:"ENV" default:"development" validate:"required"`
 
 	// Ethereum
 	EthProvider        string `validate:"required"`
-	EthProviderMainnet string `env:"ETH_PROVIDER_MAINNET" validate:"required"`
-	EthProviderKovan   string `env:"ETH_PROVIDER_KOVAN" validate:"required"`
-	EthProviderFork    string `env:"ETH_PROVIDER_FORK" validate:"required"`
+	EthProviderMainnet string `env:"ETH_PROVIDER_MAINNET"`
+	EthProviderKovan   string `env:"ETH_PROVIDER_KOVAN"`
+	EthProviderFork    string `env:"ETH_PROVIDER_FORK"`
 
 	ChainId   uint   `validate:"required"`
 	NetworkId string `env:"REACT_APP_CHAIN_ID" validate:"required"`
-
-	//TelegramBotToken string `env:"TG_BOT_TOKEN" validate:"required"`
-
-	SentryDSN string `env:"SENTRY_DSN" validate:"required"`
 
 	Port    string `env:"PORT" default:"8080" validate:"required"`
 	AMPQUrl string `env:"CLOUDAMQP_URL" validate:"required"`
