dddfb4423ae39b01afc366830abb877db94df7e9
harsh jain <harshjniitr@gmail.com>
2021-12-14 18:27:16 +0530

    Wasn't added before due to .gitignore

---
+++ cmd/application/main.go
@@ -0,0 +1,59 @@
+/*
+* Gearbox monitoring
+* Copyright (c) 2021. Mikael Lazarev
+*
+ */
+
+package main
+
+import (
+	"net/http"
+	"github.com/Gearbox-protocol/third-eye/config"
+	"github.com/Gearbox-protocol/third-eye/core"
+	"github.com/Gearbox-protocol/third-eye/engine"
+	"github.com/Gearbox-protocol/third-eye/ethclient"
+	"github.com/Gearbox-protocol/third-eye/log"
+	"github.com/Gearbox-protocol/third-eye/repository"
+	"github.com/Gearbox-protocol/third-eye/services"
+	"go.uber.org/fx"
+	"context"
+	"time"
+)
+
+func StartServer(lc fx.Lifecycle, engine core.EngineI) {
+
+	// Starting server
+	lc.Append(fx.Hook{
+		// To mitigate the impact of deadlocks in application startup and
+		// shutdown, Fx imposes a time limit on OnStart and OnStop hooks. By
+		// default, hooks have a total of 15 seconds to complete. Timeouts are
+		// passed via Go's usual context.Context.
+		OnStart: func(context.Context) error {
+			// In production, we'd want to separate the Listen and Serve phases for
+			// better error-handling.
+			go func() {
+				engine.SyncHandler()
+			}()
+			return nil
+		},
+	})
+}
+
+func main() {
+	app := fx.New(
+		ethclient.Module,
+		config.Module,
+		repository.Module,
+		services.Module,
+		engine.Module,
+		fx.NopLogger,
+		fx.Invoke(StartServer),
+	)
+	startCtx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	if err := app.Start(startCtx); err != nil {
+		log.Fatal(err)
+	}
+
+	<-app.Done()
+}
