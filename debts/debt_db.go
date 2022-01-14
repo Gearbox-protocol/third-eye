e7348a74511e7096e2eee6c2819e85a3af36071d
harsh jain <harshjniitr@gmail.com>
2022-01-14 18:28:20 +0530

    Add throttle hr

--- debts/debt_db.go
+++ debts/debt_db.go
@@ -104,7 +104,7 @@ func (eng *DebtEngine) AddDebt(debt *core.Debt, forceAdd bool) {
 		// add debt if throttle is enabled and (last debt is missing or forced add is set)
 		if lastDebt == nil || forceAdd {
 			eng.addDebt(debt)
-		} else if (debt.BlockNumber-lastDebt.BlockNumber) >= core.NoOfBlocksPerHr ||
+		} else if (debt.BlockNumber-lastDebt.BlockNumber) >= core.NoOfBlocksPerHr * eng.config.ThrottleByHrs ||
 			core.DiffMoreThanFraction(lastDebt.CalTotalValueBI, debt.CalTotalValueBI, big.NewFloat(0.05)) ||
 			core.DiffMoreThanFraction(lastDebt.CalBorrowedAmountPlusInterestBI, debt.CalBorrowedAmountPlusInterestBI, big.NewFloat(0.05)) ||
 			// add debt when the health factor is on different side of 10000 from the lastdebt
