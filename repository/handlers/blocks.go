a36c16ac19b2895d297b186e38a8adf500a46f1b
harsh jain <harshjniitr@gmail.com>
2022-09-27 14:05:35 +0530

    feature(#32): don't store duplicate prices for same token

--- repository/handlers/blocks.go
+++ repository/handlers/blocks.go
@@ -20,6 +20,9 @@ type BlocksRepo struct {
 	blocks map[int64]*schemas.Block
 	// for treasury to get the date
 	blockDatePairs map[int64]*schemas.BlockDate
+	// for prevently duplicate query price feed already with same price for a token
+	// token to feed
+	prevPriceFeeds map[bool]map[string]*schemas.PriceFeed
 	mu             *sync.Mutex
 	client         core.ClientI
 	chainId        uint
@@ -30,6 +33,7 @@ func NewBlocksRepo(db *gorm.DB, client core.ClientI, cfg *config.Config) *Blocks
 	return &BlocksRepo{
 		blocks:         make(map[int64]*schemas.Block),
 		blockDatePairs: map[int64]*schemas.BlockDate{},
+		prevPriceFeeds: map[bool]map[string]*schemas.PriceFeed{},
 		//
 		mu:      &sync.Mutex{},
 		client:  client,
@@ -136,6 +140,44 @@ func (repo *BlocksRepo) LoadBlockDatePair() {
 	for _, entry := range data {
 		repo.addBlockDate(entry)
 	}
+	repo.loadPrevPriceFeed()
+}
+
+func (repo *BlocksRepo) loadPrevPriceFeed() {
+	defer utils.Elapsed("loadPrevPriceFeed")()
+	data := []*schemas.PriceFeed{}
+	err := repo.db.Raw("SELECT distinct on(token)* FROM price_feeds ORDER BY token, block_num DESC").Find(&data).Error
+	log.CheckFatal(err)
+	for _, pf := range data {
+		repo.addPrevPriceFeed(pf)
+	}
+}
+func (repo *BlocksRepo) addPrevPriceFeed(pf *schemas.PriceFeed) {
+	if repo.prevPriceFeeds[pf.IsPriceInUSD] == nil {
+		repo.prevPriceFeeds[pf.IsPriceInUSD] = map[string]*schemas.PriceFeed{}
+	}
+	oldPF := repo.prevPriceFeeds[pf.IsPriceInUSD][pf.Token]
+	if oldPF != nil && oldPF.BlockNumber >= pf.BlockNumber {
+		log.Fatalf("oldPF %s.\n NewPF %s.", oldPF, pf)
+	}
+	repo.prevPriceFeeds[pf.IsPriceInUSD][pf.Token] = pf
+}
+
+func (repo *BlocksRepo) AddPriceFeed(pf *schemas.PriceFeed) {
+	repo.mu.Lock()
+	defer repo.mu.Unlock()
+	if repo.prevPriceFeeds[pf.IsPriceInUSD] != nil && repo.prevPriceFeeds[pf.IsPriceInUSD][pf.Token] != nil {
+		prevPF := repo.prevPriceFeeds[pf.IsPriceInUSD][pf.Token]
+		if prevPF.BlockNumber >= pf.BlockNumber {
+			log.Fatalf("oldPF %s.\n NewPF %s.", prevPF, pf)
+		}
+		if prevPF.PriceBI.Cmp(pf.PriceBI) == 0 {
+			repo.addPrevPriceFeed(pf)
+			return
+		}
+	}
+	repo.addPrevPriceFeed(pf)
+	repo._setAndGetBlock(pf.BlockNumber).AddPriceFeed(pf)
 }
 
 func (repo *BlocksRepo) RecentEventMsg(blockNum int64, msg string, args ...interface{}) {
@@ -158,10 +200,6 @@ func (repo *BlocksRepo) AddDAOOperation(operation *schemas.DAOOperation) {
 
 func (repo *BlocksRepo) AddCreditManagerStats(cms *schemas.CreditManagerStat) {
 	repo.SetAndGetBlock(cms.BlockNum).AddCreditManagerStats(cms)
-}
-
-func (repo *BlocksRepo) AddPriceFeed(pf *schemas.PriceFeed) {
-	repo.SetAndGetBlock(pf.BlockNumber).AddPriceFeed(pf)
 }
 
 func (repo *BlocksRepo) AddUniswapPrices(prices *schemas.UniPoolPrices) {
