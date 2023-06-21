6e1d385568e858f03b8131bb564632b9a5a973c5
harsh-98 <harshjniitr@gmail.com>
2023-06-21 12:20:59 +0530

    fix: update balances to close_at-1 for closed accounts in cs

--- migrations/000025_session_close_tranfers.up.sql
+++ migrations/000025_session_close_tranfers.up.sql
@@ -3,3 +3,5 @@ update credit_sessions set close_transfers=transfers FROM
     (select id, jsonb_object(ARRAY_AGG(k), ARRAY_AGG(v->>'F')) transfers FROM 
         (select id, k,v from credit_sessions, jsonb_each(balances) arr(k,v) where version=2 and status=1) tmp group by id) tmp2
         where tmp2.id=credit_sessions.id;
+
+update credit_sessions cs set  balances=css.balances from  credit_session_snapshots css where status=1 and version = 2 and  css.block_num = cs.closed_at-1 and cs.id= css.session_id;
