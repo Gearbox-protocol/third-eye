50c6b03388524fd61b4aab31dd3a9d6cfa545c01
harsh-98 <harshjniitr@gmail.com>
2024-02-23 13:08:55 +0400

    fix: increase delta for total value to be wrong

--- debts/engine.go
+++ debts/engine.go
@@ -401,7 +401,7 @@ func (eng *DebtEngine) CalculateSessionDebt(blockNum int64, session *schemas.Cre
 		}
 		// even if data compressor matching is disabled check the calc values  with session data at block where last credit snapshot was taken
 	} else if sessionSnapshot.BlockNum == blockNum && sessionSnapshot.HealthFactor.Convert().Cmp(new(big.Int)) != 0 { // it is 0 when the issuccessful is false for redstone credit accounts
-		if IsChangeMoreThanFraction(debt.CalTotalValueBI, sessionSnapshot.TotalValueBI, big.NewFloat(0.0001)) ||
+		if IsChangeMoreThanFraction(debt.CalTotalValueBI, sessionSnapshot.TotalValueBI, big.NewFloat(0.001)) ||
 			// hf value calculated are on different side of 1
 			core.ValueDifferSideOf10000(debt.CalHealthFactor, sessionSnapshot.HealthFactor) ||
 			// if healhFactor diff by 4 %
