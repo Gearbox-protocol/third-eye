87e1f51bcb5ff2dfaaa1165852e2a18a1f8dc52d
harsh-98 <harshjniitr@gmail.com>
2024-08-08 00:18:02 +0700

    feat: update pool rewards script

--- scripts/points/pool_rewards/main.go
+++ scripts/points/pool_rewards/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/Gearbox-protocol/sdk-go/core"
 	"github.com/Gearbox-protocol/sdk-go/log"
@@ -11,6 +12,7 @@ import (
 	"github.com/Gearbox-protocol/third-eye/config"
 	"github.com/Gearbox-protocol/third-eye/ethclient"
 	"github.com/Gearbox-protocol/third-eye/repository"
+	"github.com/ethereum/go-ethereum/common"
 )
 
 // DATABASE_URL
@@ -18,8 +20,8 @@ import (
 // ETHERSCAN_KEY
 
 type RewardAndTs struct {
-	Value                  float64
-	HowLongAgoFirstDeposit int64
+	Value                  []float64
+	// StartTs int64
 }
 
 type UserDeposit struct {
@@ -41,36 +43,96 @@ func main() {
 		TimeStamp int64   `gorm:"column:timestamp"`
 		Token     string  `gorm:"column:underlying_token"`
 		Event     string  `gorm:"column:event" json:"event"`
+		BlockNum int64 `gorm:"column:block_num"`
 	}{}
 	log.Info(lastblockAllowed)
 	err = db.Raw(`select p.underlying_token, a.*, b.timestamp from (select * from pool_ledger where event in  ('AddLiquidity', 'RemoveLiquidity') and pool in (select address from pools where _version=300) and block_num<?) a join blocks b on b.id=a.block_num join pools p on p.address=a.pool order by a.block_num`, lastblockAllowed).Find(&data).Error
 	log.CheckFatal(err)
-	ans := map[UserDeposit]*RewardAndTs{}
-	for _, d := range data {
-		v := float64(lastAllowedTs-d.TimeStamp) * d.Amount
-		if d.Event == "RemoveLiquidity" {
-			v = -v
-		}
-		key := UserDeposit{User: d.User, Token: d.Token}
-		if _, ok := ans[key]; !ok {
-			ans[key] = &RewardAndTs{
-				HowLongAgoFirstDeposit: lastAllowedTs - d.TimeStamp,
+
+	ans := map[string]map[string]float64{}
+	syms := core.GetTokenToSymbolByChainId(core.GetChainId(client))
+	final := map[UserDeposit]*RewardAndTs{}
+	//
+	ts:=utils.TimeToDateEndTime(time.Unix( data[0].TimeStamp,0)) 
+	dataInd :=0
+	for ts.Unix() <= lastAllowedTs {
+		for {
+			if dataInd >= len(data) {
+				break
 			}
+			curAction :=data[dataInd]
+			if curAction.TimeStamp > ts.Unix() {
+				break
+			}
+			v := curAction.Amount  
+			if curAction.Event == "RemoveLiquidity" {
+				// log.Info("here")
+				v = -v
+			}
+			if ans[curAction.Token] == nil {
+				ans[curAction.Token] = map[string]float64{}
+			}
+			ans[curAction.Token][curAction.User] += v
+			dataInd++
 		}
-		ans[key].Value += v
+		dayEndBlock, err := pkg.GetBlockNumForTs(utils.GetEnvOrDefault("ETHERSCAN_API_KEY", ""), core.GetChainId(client), ts.Unix())
+		log.CheckFatal(err)
+		log.Info(ts.Format(time.DateOnly), dayEndBlock)
+		for token, entry:= range ans {
+			var t float64 = 0
+			for user, e:= range entry {
+				t+=e
+				if final[UserDeposit{User: user, Token: token}] == nil {
+					final[UserDeposit{User: user, Token: token}] = &RewardAndTs{}
+				}
+				val := e*getPrice(client, token, dayEndBlock, ts.Unix())
+				if val > 10 {
+					a := final[UserDeposit{User: user, Token: token}].Value
+					a=   append(a, val)
+					final[UserDeposit{User: user, Token: token}].Value = a
+				}
+			}
+			price :=getPrice(client, token, dayEndBlock, ts.Unix())
+			log.Info(syms[common.HexToAddress(token)], t, price*t)
+		}
+		log.Info("####")
+		ts = ts.Add(time.Hour * 24)
 	}
 
-	store := priceFetcher.NewTokensStore(client)
-	orachel := priceFetcher.New1InchOracle(client, store, priceFetcher.URLsAndResolve{})
-	calls := orachel.GetCalls()
-	results := core.MakeMultiCall(client, lastblockAllowed, false, calls)
-	prices := orachel.GetPrices(results, lastblockAllowed, uint64(lastAllowedTs))
-	log.Info("here")
+
 	//
-	fmt.Println("User,Token,avgValue=sigma/HowLongAgoFirstDeposit,HowLongAgoFirstDeposit, sigma[deposit*(1julyts-depoitts)-withdraw*(1julyts-withdrawts)]")
-	for user, entry := range ans {
+	fmt.Println("User,Token,avgValue,noofDays")
+	for user, entry := range final {
 		// decimals := store.GetDecimals(common.HexToAddress(user.Token))
-		value := utils.GetFloat64Decimal(prices[user.Token].Convert(), 8) * entry.Value
-		fmt.Printf("%s,%s,%f,%d,%f\n", user.User, user.Token, value/float64(entry.HowLongAgoFirstDeposit), entry.HowLongAgoFirstDeposit, value)
+		// value := utils.GetFloat64Decimal(prices[user.Token].Convert(), 8) * entry.Value
+		fmt.Printf("%s,%s,%f,%d\n", user.User, user.Token, sum(entry.Value)/float64(len(entry.Value)), len(entry.Value))
 	}
+}
+
+func sum(a []float64) float64 {
+	var s float64 = 0
+	for _, v := range a {
+		s += v
+	}
+	return s
+}
+var orachel *priceFetcher.OneInchOracle
+var blockToPrice map[int64]map[string]*core.BigInt
+
+func getPrice(client core.ClientI, token string, block, ts int64) float64 {
+	if blockToPrice == nil {
+		blockToPrice = map[int64]map[string]*core.BigInt{}
+	}
+	if blockToPrice[block] == nil {
+		blockToPrice = map[int64]map[string]*core.BigInt{}
+		if orachel == nil {
+			store := priceFetcher.NewTokensStore(client)
+			orachel = priceFetcher.New1InchOracle(client, store, priceFetcher.URLsAndResolve{})
+		}
+		calls := orachel.GetCalls()
+		results := core.MakeMultiCall(client, block, false, calls)
+		prices := orachel.GetPrices(results, block, uint64(ts))
+		blockToPrice[block] = prices
+	}
+	return utils.GetFloat64Decimal(blockToPrice[block][token].Convert(), 8)
 }
