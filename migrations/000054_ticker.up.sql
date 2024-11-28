3501a6aad2487caeb5d25ff50dd84227eb11ee7d
harsh-98 <harshjniitr@gmail.com>
2024-11-28 09:10:59 +0700

    feat: update sdk-go

--- migrations/000054_ticker.up.sql
+++ migrations/000054_ticker.up.sql
@@ -2,3 +2,4 @@ create table tickers  (
     ticker varchar(42) ,
     feed varchar(42) PRIMARY KEY);
 
+insert into tickers(feed, ticker) values ('0x14497e822B70554537dB9950126461C23dC4f237','0x07299E4E806e4253727084c0493fFDf6fB2dBa3D'); -- only for arbitrum
